85a22e189e542ad3436335defad519269d605f66
yessyGarcia <56738324+yessyGarcia@users.noreply.github.com>
2019-11-14 08:51:22 -0600

    Cambio Alba Garcia

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -19,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_alert));
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
